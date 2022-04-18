dfcf645a88f862e3fde54a4d70eade6d46e41397
Oleg Butakov <butakovoleg@gmail.com>
2022-04-19 01:25:13 +0300

    Clean-up.

--- cloc_me.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cloc src/storm* src/StormRuler_* src_aux/StormRuler_Main/StormRuler_* CMakeLists.txt src_aux/StormRuler_Main/CMakeLists.txt
--- slices_plot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-awk 'BEGIN { i = 1 } $1 == "V_hat" { print i, $3, $6; i += 1 }' slices.txt > slices_plot.txt
-
-#first is computed, second -- reference..
-#plot 'slices_plot.txt' u 1:2, 'slices_plot.txt' u 1:3

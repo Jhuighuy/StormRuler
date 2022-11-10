4f3c6e71b3ee257b7a4671e189326fa5de0ac60d
Oleg Butakov <butakovoleg@gmail.com>
2022-11-10 23:25:55 +0300

    Use the `GTest` builtin `main`.

--- tests/BitternReductions.cpp
+++ tests/BitternReductions.cpp
@@ -18,7 +18,7 @@
 /// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 /// IN THE SOFTWARE.
 
-#include "_Main.hpp"
+#include "_UnitTests.hpp"
 
 #include <Storm/Bittern/Mat.hpp>
 

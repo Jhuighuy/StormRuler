1d11ed677762069344ab4283cf0f1b9c63828a14
Jhuighuy <butakovoleg@gmail.com>
2023-02-07 20:51:22 +0000

    Deploying to gh-pages from @ Jhuighuy/StormRuler@bba08b3ff5a23771a2f0f74b0fb6eff363ac9811 🚀

--- class_storm_1_1shapes_1_1_polygon.js
+++ class_storm_1_1shapes_1_1_polygon.js
@@ -1,7 +1,7 @@
 var class_storm_1_1shapes_1_1_polygon =
 [
     [ "Polygon", "class_storm_1_1shapes_1_1_polygon.html#ab5f1dde5eabfaee860939a87035baf50", null ],
-    [ "Polygon", "class_storm_1_1shapes_1_1_polygon.html#a355eda39b78c2b38e1be34ab4dac870a", null ],
+    [ "Polygon", "class_storm_1_1shapes_1_1_polygon.html#a3fa4837e6af7ff4abe5c9fe7be71cff4", null ],
     [ "edges", "class_storm_1_1shapes_1_1_polygon.html#a1ed051328d7d09892d949d8a0b60aa58", null ],
     [ "nodes", "class_storm_1_1shapes_1_1_polygon.html#a182ea516d393044ff35f5ad07c64e835", null ],
     [ "pieces", "class_storm_1_1shapes_1_1_polygon.html#a8687b90b14fe7c7e77140f87022242ff", null ],
--- class_storm_1_1shapes_1_1_triangle_strip.js
+++ class_storm_1_1shapes_1_1_triangle_strip.js
@@ -1,7 +1,7 @@
 var class_storm_1_1shapes_1_1_triangle_strip =
 [
     [ "TriangleStrip", "class_storm_1_1shapes_1_1_triangle_strip.html#a04ab96a274ec5ac258d8b9225ccce2ac", null ],
-    [ "TriangleStrip", "class_storm_1_1shapes_1_1_triangle_strip.html#a411ca5a42fbbae5657bab053e482fba1", null ],
+    [ "TriangleStrip", "class_storm_1_1shapes_1_1_triangle_strip.html#a1dd0a43f198bec9124e07d583a9ac4c2", null ],
     [ "edges", "class_storm_1_1shapes_1_1_triangle_strip.html#aedbd40fc5d4778755db619b806c8631c", null ],
     [ "nodes", "class_storm_1_1shapes_1_1_triangle_strip.html#a60f35dc5d1c2fad2b182df986dec250a", null ],
     [ "pieces", "class_storm_1_1shapes_1_1_triangle_strip.html#ae19a8fd809f53814fbbca2f39a3341c4", null ],

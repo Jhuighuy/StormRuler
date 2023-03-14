d28f4af4bdcc3c2e69e4382ab4bee02c84d38d0f
Oleg Butakov <butakovoleg@gmail.com>
2023-03-14 23:35:53 +0300

    Fix a copyright typo.

--- scripts/cloc.bat
+++ scripts/cloc.bat
@@ -1,24 +1,24 @@
 @echo off
 
-rem Copyright (C) 2020-2023 Oleg Butakov
-rem
-rem Permission is hereby granted, free of charge, to any person obtaining a copy
-rem of this software and associated documentation files (the "Software"), to
-rem deal in the Software without restriction, including without limitation the
-rem rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-rem sell copies of the Software, and to permit persons to whom the Software is
-rem furnished to do so, subject to the following conditions:
-rem
-rem The above copyright notice and this permission notice shall be included in
-rem all copies or substantial portions of the Software.
-rem
-rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-rem FITNESS FOR Allocator PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-rem SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-rem OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-rem ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-rem DEALINGS IN THE SOFTWARE.
+:: Copyright (C) 2020-2023 Oleg Butakov
+::
+:: Permission is hereby granted, free of charge, to any person obtaining a copy
+:: of this software and associated documentation files (the "Software"), to
+:: deal in the Software without restriction, including without limitation the
+:: rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+:: sell copies of the Software, and to permit persons to whom the Software is
+:: furnished to do so, subject to the following conditions:
+::
+:: The above copyright notice and this permission notice shall be included in
+:: all copies or substantial portions of the Software.
+::
+:: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+:: IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+:: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+:: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+:: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+:: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+:: SOFTWARE.
 
 rem ----------------------------------------------------------------------------
 
--- source/Storm/Vulture/ShaderNodes.glsl
+++ source/Storm/Vulture/ShaderNodes.glsl
@@ -12,11 +12,11 @@
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR Allocator PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 // -----------------------------------------------------------------------------
 
@@ -51,22 +51,22 @@ void main() {
   gl_PrimitiveID = gl_PrimitiveIDIn;
 
   position_model_space = vec2(-1.0, -1.0);
-  gl_Position = position_screen_space + 
+  gl_Position = position_screen_space +
                 vec4(point_size * position_model_space, 0.0, 0.0);
   EmitVertex();
 
   position_model_space = vec2(+1.0, -1.0);
-  gl_Position = position_screen_space + 
+  gl_Position = position_screen_space +
                 vec4(point_size * position_model_space, 0.0, 0.0);
   EmitVertex();
 
   position_model_space = vec2(-1.0, +1.0);
-  gl_Position = position_screen_space + 
+  gl_Position = position_screen_space +
                 vec4(point_size * position_model_space, 0.0, 0.0);
   EmitVertex();
 
   position_model_space = vec2(+1.0, +1.0);
-  gl_Position = position_screen_space + 
+  gl_Position = position_screen_space +
                 vec4(point_size * position_model_space, 0.0, 0.0);
   EmitVertex();
 
